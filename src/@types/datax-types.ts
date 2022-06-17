7ea5694fb56ddc3bb2d32ab668271b5b1e12ff2f
Keith Frazier <keithers98@gmail.com>
2022-06-17 11:57:41 -0700

    added did to token info type

--- src/@types/datax-types.ts
+++ src/@types/datax-types.ts
@@ -1,21 +1,21 @@
 import BigNumber from "bignumber.js";
 import { TokenList as Tlist, TokenInfo as TInfo } from "@uniswap/token-lists";
-import {Config} from "../utils/ConfigHelper"
+import { Config } from "../utils/ConfigHelper";
 
 export interface ConfigHelperConfig extends Config {
-    networkId: number
-    network: string
-    subgraphUri: string
-    explorerUri: string
-    oceanTokenSymbol: string
-    transactionBlockTimeout: number
-    transactionConfirmationBlocks: number
-    transactionPollingTimeout: number
-    gasFeeMultiplier: number
-    rbacUri?: string
-  }
+  networkId: number;
+  network: string;
+  subgraphUri: string;
+  explorerUri: string;
+  oceanTokenSymbol: string;
+  transactionBlockTimeout: number;
+  transactionConfirmationBlocks: number;
+  transactionPollingTimeout: number;
+  gasFeeMultiplier: number;
+  rbacUri?: string;
+}
 
-export type PoolTransactionType = 'swap' | 'join' | 'exit'
+export type PoolTransactionType = "swap" | "join" | "exit";
 
 export interface IMaxUnstake {
   OCEAN: BigNumber;
@@ -53,11 +53,12 @@ export interface ISwap {
 export interface ITokenDetails {
   name: string;
   symbol: string;
-  tokenAddress?:string
+  tokenAddress?: string;
 }
 
 export interface ITokenInfo extends TInfo {
   pool?: string;
+  did: string;
 }
 
 export interface ITList extends Tlist {
@@ -65,35 +66,35 @@ export interface ITList extends Tlist {
 }
 
 export interface IToken {
-    balance: BigNumber;
-    value: BigNumber;
-    info: ITokenInfo | null;
-    loading: boolean;
-    percentage: BigNumber;
-    allowance?: BigNumber;
-  }
+  balance: BigNumber;
+  value: BigNumber;
+  info: ITokenInfo | null;
+  loading: boolean;
+  percentage: BigNumber;
+  allowance?: BigNumber;
+}
 
 export interface IPoolDetails {
-    poolAddress: string
-    tokens: string[]
-  }
-  
-  export interface IPoolTransaction {
-    poolAddress: string
-    dtAddress: string
-    caller: string
-    transactionHash: string
-    blockNumber: number
-    timestamp: number
-    tokenIn?: string
-    tokenOut?: string
-    tokenAmountIn?: string
-    tokenAmountOut?: string
-    type: PoolTransactionType
-  }
+  poolAddress: string;
+  tokens: string[];
+}
 
-  export interface ITokensToAdd {
-    address: string;
-    amount: string;
-    weight: string;
-  }
+export interface IPoolTransaction {
+  poolAddress: string;
+  dtAddress: string;
+  caller: string;
+  transactionHash: string;
+  blockNumber: number;
+  timestamp: number;
+  tokenIn?: string;
+  tokenOut?: string;
+  tokenAmountIn?: string;
+  tokenAmountOut?: string;
+  type: PoolTransactionType;
+}
+
+export interface ITokensToAdd {
+  address: string;
+  amount: string;
+  weight: string;
+}
