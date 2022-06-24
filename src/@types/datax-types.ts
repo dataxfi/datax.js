3fcd2e783edf815112b14857a8f8696bb6ded16d
Keith Frazier <keithers98@gmail.com>
2022-06-24 12:36:54 -0700

    fixed type import

--- src/@types/datax-types.ts
+++ src/@types/datax-types.ts
@@ -1,6 +1,6 @@
 import BigNumber from "bignumber.js";
 import { TokenList as Tlist, TokenInfo as TInfo } from "@uniswap/token-lists";
-import { Config } from "src/models";
+import { Config } from "../models";
 
 export interface ConfigHelperConfig extends Config {
   networkId: number;
